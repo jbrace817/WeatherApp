7c6f465bfb0351daadc2901f637e4c75679b08b9
jbrace817 <48393754+jbrace817@users.noreply.github.com>
2024-04-18 11:01:07 -0400

    refactor: missed
    found a repeated property and created a customer css property. Also for got to delete a console.log message.

--- src/index.js
+++ src/index.js
@@ -6,12 +6,6 @@ import { DailyForecast } from './dailyForecast';
 import { LocationSettings } from './locationSettings';
 import './style.css';
 
-// const todaysWeather = new WeatherAPI();
-
-// todaysWeather
-//   .currentWeatherbyPos()
-//   .then((data) => console.log(data.current.temp_f));
-
 window.customElements.define('current-conditions', CurrentConditions);
 window.customElements.define('current-dashboard', Dashboard);
 window.customElements.define('hourly-scroll', HourlyScroll);
