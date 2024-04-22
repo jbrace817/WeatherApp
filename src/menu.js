01c78076b8ecb09e4a7c2d6a007fb732efaecf0a
jbrace817 <48393754+jbrace817@users.noreply.github.com>
2024-04-22 15:00:38 -0400

    feat: themes method added to settings component.
    Light and Dark mode themes working.

--- src/menu.js
+++ src/menu.js
@@ -46,7 +46,7 @@ menuTemplate.innerHTML = `
 </style>
 
 <div class="menu">
-    <div id="mode"><img src="./images/moon.svg" alt=""><p>Dark</p></div>
+    <div id="theme"><img src="./images/moon.svg" alt=""><p>Dark</p></div>
     <div><img src="./images/thermometer.svg" alt=""><p>Celsius</p></div>
     <div><img src="./images/map-pin.svg" alt="Map pin icon for location settings"><p>Locations</p></div>
 </div>
@@ -57,6 +57,38 @@ class Menu extends HTMLElement {
     super();
     this.attachShadow({ mode: 'open' });
     this.shadowRoot.appendChild(menuTemplate.content.cloneNode(true));
+  }
+
+  themes() {
+    const theme = this.shadowRoot.getElementById('theme');
+
+    theme.addEventListener('click', () => {
+      if (theme.lastElementChild.textContent === 'Dark') {
+        theme.firstElementChild.setAttribute('src', './images/sun.svg');
+        theme.lastElementChild.textContent = 'Light';
+        document.body.style.backgroundImage = 'url("./images/darkMode.jpg")';
+        document.documentElement.style.setProperty(
+          '--components-backgroundLight',
+          'rgba(47, 53, 71, 0.75)',
+        );
+        document.documentElement.style.setProperty('--menu-color', '#3d414f');
+        document.body.style.color = '#EEF2FB';
+      } else {
+        theme.firstElementChild.setAttribute('src', './images/moon.svg');
+        theme.lastElementChild.textContent = 'Dark';
+        document.body.style.backgroundImage = 'url("./images/lightMode.jpg")';
+        document.documentElement.style.setProperty(
+          '--components-backgroundLight',
+          'rgba(255, 255, 255, 0.75)',
+        );
+        document.documentElement.style.setProperty('--menu-color', '#f8fafd');
+        document.body.style.color = '#4f5867';
+      }
+    });
+  }
+
+  connectedCallback() {
+    this.themes();
   }
 }
 
